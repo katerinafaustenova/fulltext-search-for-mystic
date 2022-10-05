ecb6e6289e4910a2ee56f71f3be4017e6de7b9e6
katerinafaustenova <katerina.faustenova@seznam.cz>
2022-10-05 09:49:33 +0200

    fetch debounce, loading state, empty query

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Search.css";
 
 function App() {
-  const [loading, setLoading] = useState();
-  const [results, setResults] = useState();
-  const [error, setError] = useState();
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState(null);
+  const [error, setError] = useState(false);
 
-  const handleChange = () => {
+  const handleQueryChange = () => {
     const options = {
       method: "GET",
     };
-    // todo když user dopíše a fetch ještě nevrátí result nebo chybu, tak nasetovat loading
     fetch("https://api.chucknorris.io/jokes/random", options)
       .then((res) => res.json())
       .then((res) => {
+        setLoading(false);
         setResults(res);
       })
       .catch((err) => {
+        setLoading(false);
         setError(err);
       });
   };
 
+  useEffect(() => {
+    if (query !== "") {
+      setLoading(true);
+      const getData = setTimeout(() => {
+        handleQueryChange();
+      }, 1000);
+      return () => clearTimeout(getData);
+    } else {
+      setLoading(false);
+      setResults(null);
+    }
+  }, [query]);
+
   return (
     <div className="search">
-      <input type="text" onChange={handleChange} />
-      {results && <div className="results">{results?.value}</div>}
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      {loading && <div className="loading">Hledám...</div>}
+      {error && <div className="error">Nenalezen žádný výsledek.</div>}
+      {results && !loading && !error && (
+        <div className="results">{results?.value}</div>
+      )}
     </div>
   );
 }
