e6fca78df98a6e5d521cde2763fb3244736f93dd
katerinafaustenova <katerina.faustenova@seznam.cz>
2022-11-05 16:42:15 +0100

    rename root

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Search from "./Search";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("custom-search"));
 root.render(
   <React.StrictMode>
     <Search />
