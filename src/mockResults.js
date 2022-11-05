b6149152a7eb52f374512f99544a33c042aa2646
katerinafaustenova <katerina.faustenova@seznam.cz>
2022-11-05 16:58:43 +0100

    mock data

--- src/mockResults.js
+++ src/mockResults.js
@@ -2,28 +2,25 @@ export const mockResults = {
   results: [
     {
       id: 1,
-      title: "title",
+      title: "Chandra Ablaze",
       price: 100,
-      image:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmagic.wizards.com%2Fen&psig=AOvVaw2gAJ1Ztmj_XczBJOv7v4eV&ust=1665042842993000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjr947OyPoCFQAAAAAdAAAAABAD",
+      image: "http://mysticshop.cz/img/standard/zendikar/chandra_ablaze.jpg",
       category: "produkt",
       link: "https://mstest.cloud-fr1.unispace.io/ms-staging/mysticshop.cz/www/product/detail?code=AT11037",
     },
     {
       id: 2,
-      title: "title2",
+      title: "Garruk Wildspeaker",
       price: 200,
-      image:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmagic.wizards.com%2Fen&psig=AOvVaw2gAJ1Ztmj_XczBJOv7v4eV&ust=1665042842993000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjr947OyPoCFQAAAAAdAAAAABAD",
+      image: "http://mysticshop.cz/img/standard/lorwyn/garruk_wildspeaker.jpg",
       category: "karta",
       link: "https://mstest.cloud-fr1.unispace.io/ms-staging/mysticshop.cz/www/product/detail?code=AT11016",
     },
     {
       id: 3,
-      title: "title3",
+      title: "Ajani Goldmane",
       price: 300,
-      image:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmagic.wizards.com%2Fen&psig=AOvVaw2gAJ1Ztmj_XczBJOv7v4eV&ust=1665042842993000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjr947OyPoCFQAAAAAdAAAAABAD",
+      image: "http://mysticshop.cz/img/standard/m10/ajani_goldmane.jpg",
       category: "karta",
       link: "https://mstest.cloud-fr1.unispace.io/ms-staging/mysticshop.cz/www/product/detail?code=TEST1",
     },
